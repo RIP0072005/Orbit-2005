e336de75de74fd1fbf96f31bd3aea4be374e44d0
Ahmed Hegazy <ahmed.hegazy152300001@el-eng.menofia.edu.eg>
2026-08-12 16:06:34 +0300

    Removed unnecessary folder

--- WebApiPractice/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApiPractice.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Product>? Products { get; set; }
-    }
-}
